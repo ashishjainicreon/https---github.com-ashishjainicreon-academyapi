807e085cbac01a21cc93fe62323c7d35ef990822
ashishjain <ashish.jain2@icreon.com>
2017-03-03 18:00:32 +0530

    Master

---
+++ AcademyAPI/AcademyAPI/Userprofile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyAPI
+{
+    public class Class1
+    {
+    }
+}
